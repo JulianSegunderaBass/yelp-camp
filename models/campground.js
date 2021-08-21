a50954016369f610c1241da4c591295b2ff078ff
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2021-08-22 00:41:34 +0800

    Bootstrap style essentials

--- models/campground.js
+++ models/campground.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema; // For quicker reference
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location: String
 });
